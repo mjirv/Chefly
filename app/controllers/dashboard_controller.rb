d08878f758e6444f8bb6d83d93026dc3ffb7e6e7
Michael Irvine <michael.j.irvine@gmail.com>
2016-11-07 05:57:14 +0000

    Started updating UI

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,8 @@ class DashboardController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
-		@grocery_list_id = GroceryList.where(:user_id => @user.id).where(:status => GroceryList.statuses["active"]).first.id rescue 0 #TODO change this
+		@grocery_list_id = GroceryList.where(:user_id => @user.id).where(:status => GroceryList.statuses["active"]).first.id rescue 0
+        @grocery_list = @grocery_list_id > 0
+        @recipes_length = RecipeToUserLink.where(:user_id => @user.id).where(:status => RecipeToUserLink.statuses["active"]).count
 	end
 end
