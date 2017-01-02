8b549e6d9dcddc15c7e3c2e007d9459417f4c2cc
Michael Irvine <michael.j.irvine@gmail.com>
2017-01-02 18:08:17 -0500

    Updated deduplicate function for better memory efficiency

--- app/models/grocery_list.rb
+++ app/models/grocery_list.rb
@@ -1,51 +1,61 @@
 class GroceryList < ApplicationRecord
-	validates :user_id, presence: true
-	validates :status, presence: true
+    validates :user_id, presence: true
+    validates :status, presence: true
 
-	belongs_to :user
-	has_many :grocery_list_items
-	enum status: [ :active, :inactive ]
+    belongs_to :user
+    has_many :grocery_list_items
+    enum status: [ :active, :inactive ]
 
-	before_create do
-		@list = Hash.new([0])
-	end
+    before_create do
+        @list = Hash.new([0])
+    end
 
-	def deduplicate
-		units = GroceryListItem.where(:grocery_list_id => self.id).joins(:recipe_item).joins('INNER JOIN quantities on recipe_items.quantity_id = quantities.id').pluck('quantities.unit_id').uniq
+    def deduplicate
+        # Merges multiple grocery list items with the same item ID and unit type
+        # Get the list of GLI IDs, item IDs, unit IDs, and GLI names
+        glis = GroceryListItem.where(:grocery_list_id => self.id).where.not(:visible => false)..where.not(:combined => true).where.not(:user_edited => true).joins(:recipe_item).joins('INNER JOIN quantities on recipe_items.quantity_id = quantities.id').select('grocery_list_items.id AS id, grocery_list_items.name AS name, quantities.unit_id AS unit_id, grocery_list_items.recipe_item_id AS recipe_item_id, recipe_items.item_id as item_id')
 
-		units.each do |unit|
-			glis = GroceryListItem.where(:grocery_list_id => self.id).where(:visible => true).where(:recipe_item_id => RecipeItem.where(:quantity_id => Quantity.where(:unit_id => unit).pluck(:id)).pluck(:id))
-			items = glis.map(&:recipe_item).pluck(:item_id).uniq
-			items.each do |item|
-				item_glis = glis.select{ |g| RecipeItem.find(g.recipe_item_id).item_id == item }
-				if item_glis.length > 1
-					sum = item_glis.map{ |g| g.amount }.reduce(0.0, :+)
-					new_gli = GroceryListItem.create(:amount => sum, :string_amount => sum.to_s, :name => item_glis.first.name, :grocery_list_id => self.id, :recipe_item_id => item_glis.first.recipe_item_id, :combined => true)
-					item_glis.map{ |i| i.visible = false }
-					item_glis.map(&:save)
-				end
-			end
-		end
-	end
+        # Loop through each of the glis
+        while glis.length != 0
+            # "Pop" the first one
+            gli = glis.shift
 
-	def get_list
-		list = []
-		self.grocery_list_items.each do |item|
-			list << item.to_s
-		end
-		list
-	end
+            # Pull those with same item ID and unit ID
+            mergeable_glis = glis.select{ |candidate| candidate.item_id == gli.item_id && candidate.unit_id == gli.unit_id }
 
-	def grocery_list_items
-		return GroceryListItem.where(:grocery_list_id => self.id).where.not(:visible => false)
-	end
+            # Get merged amount
+            new_amount = mergeable_glis.map{ |mgli| mgli.amount }.reduce(:+) + gli.amount
 
-	def regenerate_items
-		glis = grocery_list_items
-		glis.each do |gli|
-			if gli.combined != false && gli.user_edited != true
-				gli.regenerate()
-			end
-		end
-	end
+            # Add the merged GLI
+            new_merged_gli = GroceryListItem.new(:name => gli.name, :amount => new_amount, :grocery_list_id => gli.grocery_list_id, :recipe_item_id => gli.recipe_item_id, :item_name => gli.item_name, :visible => true, :combined => true, :user_edited => false)
+
+            # Make the earlier ones invisible
+            mglis_ids = mergeable_glis.map(&:id) + [gli.id]
+            GroceryListItem.find(mglis_ids).map do |g|
+                g.visible = false
+                g.save
+            end
+        end
+    end
+
+    def get_list
+        list = []
+        self.grocery_list_items.each do |item|
+            list << item.to_s
+        end
+        list
+    end
+
+    def grocery_list_items
+        return GroceryListItem.where(:grocery_list_id => self.id).where.not(:visible => false)
+    end
+
+    def regenerate_items
+        glis = grocery_list_items
+        glis.each do |gli|
+            if gli.combined != false && gli.user_edited != true
+                gli.regenerate()
+            end
+        end
+    end
 end
