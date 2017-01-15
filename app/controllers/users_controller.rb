994511e06dd82b0b341f1e34c7f4adda6c8c25a7
Michael Irvine <michael.j.irvine@gmail.com>
2017-01-14 21:38:49 -0800

    typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -80,7 +80,7 @@ class UsersController < ApplicationController
                     g.save
                 end
                 RecipeToUserLink.where(:status => RecipeToUserLink.statuses["active"]).where(:user_id => user_id).map do |rul|
-                    rul.status = RecupeToUserLink.statuses["inactive"]
+                    rul.status = RecipeToUserLink.statuses["inactive"]
                     rul.save
                 end
                 generate_new_grocery_list(user_id, redirect_to_gl)
